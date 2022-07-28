9335427e45df7dbebc42ebae95e555f560bdfa31
BrundaBR <revadibrunda@gmail.com>
2022-07-28 17:02:09 +0530

    changes acc to new excel format

--- Automate_form_filling.py
+++ Automate_form_filling.py
@@ -10,27 +10,29 @@ browser.set_window_size(900,900)
 #Work on excel= read data from excel
 df=pd.ExcelFile('demofile.xlsm')
 df1=pd.read_excel(df,'Site Contents')
-#enter in wesite form/share point
-browser.get('file:///home/brunda/Desktop/Projects/CMT_Automate/form.html')
 
-for i in range(1,len(df1)):
-    
+#enter in wesite form/share point
+for i in range(1,len(df1)):   
     #get excel data from each column
     data=df1.iloc[i]
+    #Adding link to portal where data needs to be added, example DE
+    browser.get('file:///home/brunda/Desktop/Projects/CMT_Automate/form.html')
     mkpl=browser.find_element("name","mkpl").send_keys(data[0])
-    pl=browser.find_element("name","pl").send_keys(data[1])
-    gl=browser.find_element("name","gl").send_keys(data[2])
-    cp=browser.find_element("name","cp").send_keys(data[3])
-    setup=browser.find_element("name","setup").send_keys(data[4])
-    sd=browser.find_element("name","date").send_keys(str(data[5]))
-    ed=browser.find_element("name","date1").send_keys(str(data[6]))
-    url=browser.find_element("name","url").send_keys(data[7])
-    code=browser.find_element("name","code").send_keys(data[8])
-    offer=browser.find_element("name","offer").send_keys(data[9])
-    match=browser.find_element("name","match").send_keys(data[10])
-    rebate=browser.find_element("name","rebate").send_keys(data[11])
-    time.sleep(3)
-    browser.find_element("name","submit").click()#submit data 
+    if str(data[0]).upper() =="DE": 
+        print("DE region promotion")
+        pl=browser.find_element("name","pl").send_keys(data[1])
+        gl=browser.find_element("name","gl").send_keys(data[2])
+        cp=browser.find_element("name","cp").send_keys(data[3])
+        setup=browser.find_element("name","setup").send_keys(data[4])
+        sd=browser.find_element("name","date").send_keys(str(data[5]))
+        ed=browser.find_element("name","date1").send_keys(str(data[6]))
+        url=browser.find_element("name","url").send_keys(data[7])
+        code=browser.find_element("name","code").send_keys(data[8])
+        offer=browser.find_element("name","offer").send_keys(data[9])
+        match=browser.find_element("name","match").send_keys(data[10])
+        rebate=browser.find_element("name","rebate").send_keys(data[11])
+        time.sleep(3)
+        browser.find_element("name","submit").click()#submit data 
 
-time.sleep(10) #delay
+time.sleep(2) #delay
 browser.quit()
--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 ## problem
 The recuring process of adding data/promotion captured in file to portal either stark or sharepoint is time consuming.<br>
-time consumed for this process is approximately 5 minutes and 25 minutes for a week.
+Estimate:
+Time consumed for this process is approximately 5 minutes per day and 25 minutes for a week, which is not particularly efficient.
 
 ## Solution offered
-The application will only require adding file which will be input and the data will automatically get stored in portal.
+Solution i am proposing is to automate the process of manually adding data to portal such as sharepoint or stark.
+
+End goal:
+The  application will only require adding file to application and the data will automatically get stored in portal.
+
 
 ## Disadvantages
 
